282b421e8b96259f9333c0c58c4d40cca94d1175
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2024-11-11 03:29:47 +0800

    Update.

--- src/DataImport/DataImporter.hpp
+++ src/DataImport/DataImporter.hpp
@@ -1,120 +1,130 @@
 #pragma once
 
 #include <rapidcsv.h>
-#include <Util/Util.hpp>
-#include <filesystem>
 
 #include <Database/SqliteSchema.hpp>
+#include <Util/Util.hpp>
+#include <filesystem>
 
 class DataImporter {
 public:
 	std::vector<std::filesystem::path> m_TissueFolders;
 
-	void importRecordFromFolder(const std::filesystem::path& folderPath){
-		for (const auto& tissueFolderEntry : std::filesystem::directory_iterator(folderPath)){
-            if (tissueFolderEntry.is_directory()){
-                m_TissueFolders.emplace_back(tissueFolderEntry.path());
-            }
-        }
+	void importRecordFromFolder(const std::filesystem::path& folderPath) {
+		for (const auto& tissueFolderEntry :
+			 std::filesystem::directory_iterator(folderPath)) {
+			if (tissueFolderEntry.is_directory()) {
+				m_TissueFolders.emplace_back(tissueFolderEntry.path());
+			}
+		}
 
 		for (const auto& tissueFolder : m_TissueFolders) {
-		    importCellTissueFile(tissueFolder);
+			importCellTissueFile(tissueFolder);
 		}
 	}
 
-	void importCellTissueFile(const std::filesystem::path& tissueFolder){
-            std::filesystem::path cellMapFilePath;
-			std::filesystem::path cellTissueFilePath;
-		    int date = 0;
-		    int time = 0;
-		    std::vector<std::pair<int, std::filesystem::path>> stateMachineDifferencesFiles;
-            std::map<int, std::filesystem::path> cellMap;
+	void importCellTissueFile(const std::filesystem::path& tissueFolder) {
+		std::filesystem::path cellMapFilePath;
+		std::filesystem::path cellTissueFilePath;
+		int date = 0;
+		int time = 0;
+		std::vector<std::pair<int, std::filesystem::path>>
+			stateMachineDifferencesFiles;
+		std::map<int, std::filesystem::path> cellMap;
 
-            for (const auto& recordFolderEntry : std::filesystem::directory_iterator(tissueFolder)){
-                if (recordFolderEntry.is_directory()){
-                    auto fileName = recordFolderEntry.path().stem().string();
-                    auto splitResult = StringSplit(fileName, '_');
+		for (const auto& recordFolderEntry :
+			 std::filesystem::directory_iterator(tissueFolder)) {
+			if (recordFolderEntry.is_directory()) {
+				auto fileName = recordFolderEntry.path().stem().string();
+				auto splitResult = StringSplit(fileName, '_');
 
-                    if(splitResult.size() == 3) {
-                        int cellId;
-                        try {
-                            cellId = std::stoi(splitResult[splitResult.size() - 1]);
+				if (splitResult.size() == 3) {
+					int cellId;
+					try {
+						cellId = std::stoi(splitResult[splitResult.size() - 1]);
 
-                            auto iter = cellMap.find(cellId);
-                            if (iter != cellMap.end()) {
-                                int curDate = std::stoi(splitResult[0]);
-                                int curTime = std::stoi(splitResult[1]);
+						auto iter = cellMap.find(cellId);
+						if (iter != cellMap.end()) {
+							int curDate = std::stoi(splitResult[0]);
+							int curTime = std::stoi(splitResult[1]);
 
-                                auto lastFileName = iter->second.stem().string();
-                                auto sp = StringSplit(lastFileName, '_');
-                                int lastDate = std::stoi(sp[0]);
-                                int lastTTime = std::stoi(sp[0]);
+							auto lastFileName = iter->second.stem().string();
+							auto sp = StringSplit(lastFileName, '_');
+							int lastDate = std::stoi(sp[0]);
+							int lastTTime = std::stoi(sp[0]);
 
-                                if(curDate > lastDate || (curDate == lastDate && curTime > lastTTime)){
-                                    cellMap[cellId] = recordFolderEntry.path();
-                                }
-                            } else {
-                                cellMap.emplace(cellId, recordFolderEntry.path());
-                            }
-                        }catch (std::exception& e) {
+							if (curDate > lastDate ||
+								(curDate == lastDate && curTime > lastTTime)) {
+								cellMap[cellId] = recordFolderEntry.path();
+							}
+						} else {
+							cellMap.emplace(cellId, recordFolderEntry.path());
+						}
+					} catch (std::exception& e) {
+					}
+				}
+			} else if (recordFolderEntry.is_regular_file()) {
+				if (recordFolderEntry.path().extension() == ".csv") {
+					auto fileName = recordFolderEntry.path().stem().string();
+					if (fileName.find(tissueFolder.filename().string()) !=
+						std::string::npos) {
+						auto splitResult = StringSplit(fileName, '_');
+						int curDate = 0;
+						int curTime = 0;
+						try {
+							curDate =
+								std::stoi(splitResult[splitResult.size() - 2]);
+							curTime =
+								std::stoi(splitResult[splitResult.size() - 1]);
+						} catch (std::exception& e) {
+						}
+						if (curDate > date ||
+							(curDate == date && curTime > time)) {
+							date = curDate;
+							time = curTime;
+							cellTissueFilePath = recordFolderEntry.path();
+						}
+					} else if (fileName.find("state_time_differences") !=
+							   std::string::npos) {
+						auto splitResult = StringSplit(fileName, '_');
+						int needleId = -1;
+						try {
+							needleId =
+								std::stoi(splitResult[splitResult.size() - 1]);
+						} catch (std::exception& e) {
+						}
+						if (needleId != -1) {
+							stateMachineDifferencesFiles.emplace_back(
+								needleId, recordFolderEntry.path());
+						}
+					}
+				} else if (recordFolderEntry.path().extension() == ".json") {
+					auto fileName = recordFolderEntry.path().stem().string();
+					if (fileName.find("CellMap") != std::string::npos) {
+						cellMapFilePath = recordFolderEntry.path();
+					}
+				}
+			}
+		}
 
-                        }
-                    }
-                }else if(recordFolderEntry.is_regular_file()){
-                    if (recordFolderEntry.path().extension() == ".csv"){
-						auto fileName = recordFolderEntry.path().stem().string();
-                        if(fileName.find(tissueFolder.filename().string()) != std::string::npos){
-                            auto splitResult = StringSplit(fileName, '_');
-                            int curDate = 0;
-                            int curTime = 0;
-                            try {
-                                 curDate = std::stoi(splitResult[splitResult.size() - 2]);
-                                 curTime = std::stoi(splitResult[splitResult.size() - 1]);
-                            }catch (std::exception& e) {
-                            }
-                            if(curDate > date || (curDate == date && curTime > time)) {
-                                date = curDate;
-                                time = curTime;
-                                cellTissueFilePath = recordFolderEntry.path();
-                            }
-                        }else if (fileName.find("state_time_differences") != std::string::npos){
-                            auto splitResult = StringSplit(fileName, '_');
-                            int needleId = -1;
-                            try {
-                                needleId = std::stoi(splitResult[splitResult.size() - 1]);
-                            }catch (std::exception& e) {
-                            }
-                        if(needleId!=-1){
-                                stateMachineDifferencesFiles.emplace_back(needleId, recordFolderEntry.path());
-                            }
+		auto db = SqliteSchema::getDBStorage();
+		db.sync_schema();
 
-                        }
-                    }else if(recordFolderEntry.path().extension() == ".json"){
-                        auto fileName = recordFolderEntry.path().stem().string();
-                        if (fileName.find("CellMap") != std::string::npos) {
-                            cellMapFilePath = recordFolderEntry.path();
-                        }
-                    }
-                }
-            }
+		auto cellTissueInfos = ReadCellTissueListFromFile(cellTissueFilePath);
+		for (auto& cellTissueInfo : cellTissueInfos) {
+			try {
+				db.insert(cellTissueInfo);
+			} catch (std::exception& e) {
+				std::cout << e.what() << std::endl;
+			}
+		}
 
-		    auto db = SqliteSchema::getDBStorage();
-		    db.sync_schema();
-
-            auto cellTissueInfos = ReadCellTissueListFromFile(cellTissueFilePath);
-            for(auto& cellTissueInfo : cellTissueInfos){
-                db.insert(cellTissueInfo);
-            }
-
-		    for (auto& [needleName, stateFile] : stateMachineDifferencesFiles) {
-		        auto state = ReadStateTimeDifferenceInfoListFromFile(stateFile);
-		        for(auto& stateInfo : state){
-		            stateInfo.TissueName = tissueFolder.filename().string();
-                    db.insert(stateInfo);
-                }
-		    }
+		for (auto& [needleName, stateFile] : stateMachineDifferencesFiles) {
+			auto state = ReadStateTimeDifferenceInfoListFromFile(stateFile);
+			for (auto& stateInfo : state) {
+				stateInfo.TissueName = tissueFolder.filename().string();
+				db.insert(stateInfo);
+			}
+		}
 	}
 };
-
-
-
--- src/FileReader/CellMapInfo.hpp
+++ src/FileReader/CellMapInfo.hpp
@@ -1,43 +1,53 @@
 #pragma once
 
-#include "json.hpp"
+#include <json.hpp>
 #include <vector>
 
 struct CellMapInfo {
-    int id = -1;
-    std::string TissueName;
+	int id = -1;
+	std::string TissueName;
 
-    int              MapIndex = 0;
-    float            MapBeginCordX = 0;
-    float            MapBeginCordY = 0;
-    float            MapEndCordX = 0;
-    float            MapEndCordY = 0;
-    int              PathIndex = 0;
-    float            SurfaceZ = 0;
-    std::vector<int> needleIndexs;
+	int MapIndex = 0;
+	float MapBeginCordX = 0;
+	float MapBeginCordY = 0;
+	float MapEndCordX = 0;
+	float MapEndCordY = 0;
+	int PathIndex = 0;
+	float SurfaceZ = 0;
+	std::vector<int> needleIndexs;
 
-    friend void to_json(nlohmann::json& jsonObject, const CellMapInfo& dataObject) {
-        jsonObject["MapIndex"] = dataObject.MapIndex;
-        jsonObject["MapBeginCordX"] = dataObject.MapBeginCordX;
-        jsonObject["MapBeginCordY"] = dataObject.MapBeginCordY;
-        jsonObject["MapEndCordX"] = dataObject.MapEndCordX;
-        jsonObject["MapEndCordY"] = dataObject.MapEndCordY;
-        jsonObject["PathIndex"] = dataObject.PathIndex;
-        jsonObject["SurfaceZ"] = dataObject.SurfaceZ;
-        jsonObject["needleIndexs"] = dataObject.needleIndexs;
-    }
+	friend void to_json(nlohmann::json& jsonObject,
+						const CellMapInfo& dataObject) {
+		jsonObject["MapIndex"] = dataObject.MapIndex;
+		jsonObject["MapBeginCordX"] = dataObject.MapBeginCordX;
+		jsonObject["MapBeginCordY"] = dataObject.MapBeginCordY;
+		jsonObject["MapEndCordX"] = dataObject.MapEndCordX;
+		jsonObject["MapEndCordY"] = dataObject.MapEndCordY;
+		jsonObject["PathIndex"] = dataObject.PathIndex;
+		jsonObject["SurfaceZ"] = dataObject.SurfaceZ;
+		jsonObject["needleIndexs"] = dataObject.needleIndexs;
+	}
 
-    friend void from_json(const nlohmann::json& jsonObject, CellMapInfo& dataObject) {
-        CellMapInfo defaultDataObject;
-        dataObject.MapIndex = jsonObject.value("MapIndex", defaultDataObject.MapIndex);
-        dataObject.MapBeginCordX = jsonObject.value("MapBeginCordX", defaultDataObject.MapBeginCordX);
-        dataObject.MapBeginCordY = jsonObject.value("MapBeginCordY", defaultDataObject.MapBeginCordY);
-        dataObject.MapEndCordX = jsonObject.value("MapEndCordX", defaultDataObject.MapEndCordX);
-        dataObject.MapEndCordY = jsonObject.value("MapEndCordY", defaultDataObject.MapEndCordY);
-        dataObject.PathIndex = jsonObject.value("PathIndex", defaultDataObject.PathIndex);
-        dataObject.SurfaceZ = jsonObject.value("SurfaceZ", defaultDataObject.SurfaceZ);
-        dataObject.needleIndexs = jsonObject.value("needleIndexs", defaultDataObject.needleIndexs);
-    }
+	friend void from_json(const nlohmann::json& jsonObject,
+						  CellMapInfo& dataObject) {
+		CellMapInfo defaultDataObject;
+		dataObject.MapIndex =
+			jsonObject.value("MapIndex", defaultDataObject.MapIndex);
+		dataObject.MapBeginCordX =
+			jsonObject.value("MapBeginCordX", defaultDataObject.MapBeginCordX);
+		dataObject.MapBeginCordY =
+			jsonObject.value("MapBeginCordY", defaultDataObject.MapBeginCordY);
+		dataObject.MapEndCordX =
+			jsonObject.value("MapEndCordX", defaultDataObject.MapEndCordX);
+		dataObject.MapEndCordY =
+			jsonObject.value("MapEndCordY", defaultDataObject.MapEndCordY);
+		dataObject.PathIndex =
+			jsonObject.value("PathIndex", defaultDataObject.PathIndex);
+		dataObject.SurfaceZ =
+			jsonObject.value("SurfaceZ", defaultDataObject.SurfaceZ);
+		dataObject.needleIndexs =
+			jsonObject.value("needleIndexs", defaultDataObject.needleIndexs);
+	}
 };
 
 using CellMapInfoList = std::vector<CellMapInfo>;
